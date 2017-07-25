40e8b0feff5534db6c04232075f65684355f10ae
John Phillips <john@moonsault.co>
2017-07-25 14:53:22 -0600

    replace essay menu with page

--- _includes/index.js
+++ _includes/index.js
@@ -9,23 +9,6 @@ $(function() {
   open_new();
   links_checkbox.change(open_new);
   
-  //hide it by default
-  $('#essay-menu').addClass('top');
-  $('#essay-menu').removeClass('bottom');
-
-  // click  to show essay menu
-  $( '#essays' ).click(function(e) {
-    $('#essay-menu').removeClass('top');
-    $('#essay-menu').addClass('view');
-    e.preventDefault();
-  });
-  
-  //click close x in menu
-  $( '#close-menu' ).click(function(e) {
-    $('#essay-menu').addClass('top');
-    $('#essay-menu').removeClass('view');
-    e.preventDefault();
-  });
   
   function open_new() {
 	var new_window = links_checkbox.prop( "checked" );
