30e5824bccfe41a95040cc4ac00621a51b4b7398
John Phillips <john@moonsault.co>
2018-05-16 11:01:37 -0600

    blogging landing page

---
+++ _includes/form.js
@@ -0,0 +1,71 @@
+$(function() {
+	// hide
+	$( "#contact" ).slideUp(0);
+
+	$( ".cta" ).bind( "click", function(e) {	  
+	  if (e.currentTarget.className.indexOf("prevent") !== -1) {
+		  //console.log($(window).height())
+		  if ($(window).height() > 1000 ) {
+			  e.preventDefault();
+		  }
+	  }
+	  $( "#contact" ).slideDown();
+	});
+	
+	// form validation
+	$( "#contact-info" ).bind( "blur", function(e) {
+		var input=$(this);
+		checkInput(input);
+	});
+	$( "#contact-msg" ).bind( "blur", function(e) {
+		var input=$(this);
+		checkInput(input);
+	});
+	
+	$( "#submit" ).bind( "click", function(e) {	  
+		var input=$("#contact-info");
+		checkInput(input);
+		var area=$("#contact-msg");
+		checkInput(area);
+		
+		var errors = $("#contact form .error:visible")
+		//console.log(errors.length)
+		
+		if (errors.length > 0) {
+			e.preventDefault();
+		} else {
+			
+		}
+	});
+	
+	function checkInput(input) {
+		//console.log(input);
+		//var input=$(this);
+		
+		if (input.val()){
+			input.removeClass("error");
+			hideError(input.attr("id"));
+		} else {
+			input.addClass("error");
+			showError(input.attr("id"));
+		}
+	};
+	
+	function showError(id) {
+		if (id == "contact-info") { 
+			$("#contact-info-error").removeClass("hidden");
+			$("#contact form .help").addClass("hidden");
+		};
+		if (id == "contact-msg") { $("#contact-msg-error").removeClass("hidden")};
+	};
+	
+	function hideError(id) {
+		if (id == "contact-info") { 
+			$("#contact-info-error").addClass("hidden");
+			$("#contact form .help").removeClass("hidden");
+		};
+		if (id == "contact-msg") { $("#contact-msg-error").addClass("hidden")};
+	};
+	
+	
+});
---
+++ thanks.md
@@ -0,0 +1,24 @@
+---
+layout: default
+title: Thanks For Contacting  Me
+---
+<header>
+   <nav>
+	 <a href="/">Infomongo</a>
+	 <a href="/tip" class="active">Thank You</a>
+   </nav>
+</header>
+
+
+<section class="white" markdown="1">
+	
+# Thanks for contacting me
+
+I will respond to your message shortly.
+
+<img src="/img/fox-field-glitch.jpg" class="full-width" alt="A surreal, digitially altered,  image of a fox in field">
+	
+	
+</section>	
+
+
